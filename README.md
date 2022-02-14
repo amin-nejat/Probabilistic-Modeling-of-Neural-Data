48c7245f6981fefcc65bec4a6e2191ca65671dbb
Amin Nejatbakhsh <amin-nejat@github.com>
2022-02-13 22:24:36 -0500

    visualization.py added, README updated

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-Download and install anaconda
-https://www.anaconda.com/products/individual
+# Installation Instructions for the Workshop on Probabilistic Modeling of Neural Data
 
-Install pytorch
-https://pytorch.org/
+1. Download and install [**anaconda**](https://docs.anaconda.com/anaconda/install/index.html)
 
-Install pyro
-https://anaconda.org/conda-forge/pyro-ppl
+2. Create a **virtual environment** using anaconda and activate it
+```
+conda create -n pyro
+conda activate pyro
+```
+	- This helps with avoiding package conflicts
+
+3. Install [**pytorch**](https://pytorch.org/get-started/locally/) with conda
+
+	- Make sure that the pytorch version is compatible with your device)
+	- If you have GPUs available on your device, use the right cuda version, check your cuda version using 
+	```
+	nvcc --version
+	```
+
+4. Install [**pyro**](https://anaconda.org/conda-forge/pyro-ppl) with conda
+
+5. Run **jypter lab** in conda and execute notebooks
+```
+jupyter lab
+```
---
+++ code/visualization.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 18:35:46 2022
+
+@author: Amin
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import logging
+
+logging.getLogger('matplotlib.font_manager').disabled = True
+
+# %%
+def plot_signals(x,z=None,titlestr='',fontsize=12,linewidth=2,save=False,file=None):
+    plt.figure(figsize=(10,3))
+    
+    offset = np.append(0.0, np.nanmax(x[:,0:-1,],0)-np.nanmin(x[:,0:-1],0))
+    s = (x-np.nanmin(x,0)[None,:]+np.cumsum(offset)[None,:])
+    
+    plt.plot(s,linewidth=linewidth)
+    
+    if z is not None:
+        colors = plt.cm.hsv(np.linspace(0,1,max(z)+2)[0:-1])[:,0:3]
+        t = np.arange(len(z))
+        for z_ in range(max(z)+1):
+            plt.vlines(x=t[z==z_], ymin=(s.min()-1), ymax=(s.max()+1), 
+                       color=colors[z_], alpha=.1, lw=1)
+
+    plt.yticks(s[0,:],[str(signal_idx) for signal_idx in range(s.shape[1])])
+        
+    if save:
+        plt.savefig(file+'.png',format='png')
+        plt.savefig(file+'.pdf',format='pdf')
+        plt.close('all')
+    else:
+        plt.show()
+
+# %%
+def plot_grad_norms(gradient_norms):
+    plt.figure(figsize=(10,4), dpi=100).set_facecolor('white')
+    for name, grad_norms in gradient_norms.items():
+        print(grad_norms)
+        plt.plot(grad_norms, label=name)
+
+    plt.xlabel('iters')
+    plt.ylabel('gradient norm')
+    plt.yscale('log')
+    plt.legend(loc='best')
+    plt.title('Gradient norms during SVI');
+
+    plt.show()
+    
+# %%
+def plot_loss(losses):
+    plt.figure(figsize=(10,3), dpi=100).set_facecolor('white')
+    plt.plot(losses)
+    plt.xlabel('iters')
+    plt.ylabel('loss')
+    plt.yscale('log')
+    plt.title('Convergence of SVI')
+    plt.show()
+
+# %%
+def plot_gmm_2d(data,mu,pi,z,weights=None,locs=None):
+    colors = plt.cm.hsv(np.linspace(0,1,max(z)+2)[0:-1])[:,0:3]
+
+    plt.scatter(data[:,0],data[:,1],s=.5,c=colors[z])
+    plt.scatter(mu[:,0],mu[:,1],s=pi*1000,c=(0,0,0,0),marker='o',edgecolors='r')
+    
+    if locs is not None:
+        plt.scatter(locs[:,0],locs[:,1],s=weights*1000,c='w',marker='o',edgecolors='k')
+
+    plt.axis('equal')
+    plt.show()
+    
